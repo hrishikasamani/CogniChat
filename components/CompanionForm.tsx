d55ff38884bf1974e5f55b3727f5b1d44a1a15a7
hrishikasamani <samani.hr@northeastern.edu>
2024-12-01 10:27:47 -0500

    edits

--- components/CompanionForm.tsx
+++ components/CompanionForm.tsx
@@ -1,3 +1,13 @@
+'use client'
+
+import { zodResolver } from '@hookform/resolvers/zod'
+import { useForm } from 'react-hook-form'
+import { z } from 'zod'
+
+const formSchema = z.object({
+    username: z.string().min(minLength: 2).max(maxLength: 50),
+})
+
 const CompanionForm = () => {
   return (
     <div>
