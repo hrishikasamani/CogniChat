636bf8b6307aed79a27416141fb6bb4c5ac1d3d2
hrishikasamani <samani.hr@northeastern.edu>
2025-04-08 12:33:39 -0400

    companion card

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,17 @@
+import CompanionCard from '@/components/CompanionCard'
 import { Button } from '@/components/ui/button'
 import React from 'react'
 
 const Page = () => {
   return (
-    <div>
-      <h1 className='text-2xl underline'>Welcome to CogniChat, my SaaS App</h1>
-      <Button>
-        Let's get started
-      </Button>
-    </div>
+    <main>
+      <h1 className='text-2xl underline'>Popular Companions</h1>
+      <section className='home-section'>
+        <CompanionCard />
+        <CompanionCard />
+        <CompanionCard />
+      </section>
+    </main>
   )
 }
 
