c7f3a25349b982c05196d2413338bddb65fb7fbe
hrishikasamani <samani.hr@northeastern.edu>
2025-06-20 15:31:38 -0400

    setup nextjs

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,14 @@
+import { Button } from '@/components/ui/button'
 import React from 'react'
 
 const Page = () => {
   return (
-    <h1 className='text-2xl underline'>Welcome to CogniChat, my SaaS App</h1>
+    <div>
+      <h1 className='text-2xl underline'>Welcome to CogniChat, my SaaS App</h1>
+      <Button>
+        Let's get started
+      </Button>
+    </div>
   )
 }
 
