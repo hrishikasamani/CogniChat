46bab2d31489f610ebdbb741bab4b8f893a73f00
hrishikasamani <samani.hr@northeastern.edu>
2025-06-21 17:16:08 -0400

    setup routes

--- app/layout.tsx
+++ app/layout.tsx
@@ -8,7 +8,7 @@ const bricolage = Bricolage_Grotesque({
 });
 
 export const metadata: Metadata = {
-  title: "Converso",
+  title: "CogniChat",
   description: "Real-time AI Teaching Platform",
 };
 
