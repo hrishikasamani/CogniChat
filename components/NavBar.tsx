fc933f9eda8606732fbee88f771d8e544fbe3a6b
hrishikasamani <samani.hr@northeastern.edu>
2025-06-22 11:50:01 -0400

    navbar

--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -6,20 +6,20 @@ import NavItems from './NavItems'
 const NavBar = () => {
   return (
     <nav className='navbar'>
-        <Link href='/'>
-            <div className='flex items-center gap-2.5 cursor-pointer'>
-                <Image
-                    src='/images/logo.svg'
-                    alt='logo'
-                    width={46}
-                    height={44}
-                />
-            </div>
-        </Link>
-        <div className='flex items-center gap-8'>
-            <NavItems/>
-            <p>Sign In</p>
-        </div>
+      <Link href='/'>
+					<div className='flex items-center gap-2.5 cursor-pointer'>
+						<Image
+							src='/images/logo.svg'
+							alt='logo'
+							width={46}
+							height={44}
+						/>
+					</div>
+      </Link>
+			<div className='flex items-center gap-8'>
+				<NavItems/>
+				<p>Sign In</p>
+			</div>
     </nav>
   )
 }
--- components/NavItems.tsx
+++ components/NavItems.tsx
@@ -1,19 +1,20 @@
 import Link from "next/link"
 
 const navItems = [
-    { label: 'Home', href: '/' },
-    { label: 'Companions', href: '/companions' },
-    { label: 'My Journey', href: '/my-journey' },
+	{ label: 'Home', href: '/' },
+	{ label: 'Companions', href: '/companions' },
+	{ label: 'My Journey', href: '/my-journey' },
 ]
 
 const NavItems = () => {
+
   return (
     <nav className='flex items-center gap-4'>
-        {navItems.map(({ label, href}) => (
-            <Link href={href} key={label}>
-                {label}
-            </Link>
-        ))}
+			{navItems.map(({ label, href}) => (
+				<Link href={href} key={label}>
+					{label}
+				</Link>
+			))}
     </nav>
   )
 }
