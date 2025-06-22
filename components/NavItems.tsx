38f316ac32b2302e3aea8c57a072a8dc2a678ff0
hrishikasamani <samani.hr@northeastern.edu>
2025-06-22 11:46:57 -0400

    navbar

--- components/NavItems.tsx
+++ components/NavItems.tsx
@@ -1,10 +1,20 @@
-import React from 'react'
+import Link from "next/link"
+
+const navItems = [
+    { label: 'Home', href: '/' },
+    { label: 'Companions', href: '/companions' },
+    { label: 'My Journey', href: '/my-journey' },
+]
 
 const NavItems = () => {
   return (
-    <div>
-      
-    </div>
+    <nav className='flex items-center gap-4'>
+        {navItems.map(({ label, href}) => (
+            <Link href={href} key={label}>
+                {label}
+            </Link>
+        ))}
+    </nav>
   )
 }
 
