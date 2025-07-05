feb7558de1f193fdb4601f017ca4f3d1de62804e
hrishikasamani <samani.hr@northeastern.edu>
2025-07-05 00:15:31 -0400

    edits

--- components/CTA.tsx
+++ components/CTA.tsx
@@ -5,6 +5,7 @@ const CTA = () => {
       <h2 className='text-3xl font-bold'>
         Build and Personalize Learning Companion
       </h2>
+      <p>Pick a name, voice & personality - and start learning through voice conversations</p>
     </section>
   )
 }
