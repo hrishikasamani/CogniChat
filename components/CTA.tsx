5ce500832155ec9414fb56e645cf07fa0033f646
hrishikasamani <samani.hr@northeastern.edu>
2025-07-05 00:13:46 -0400

    CTA

--- components/CTA.tsx
+++ components/CTA.tsx
@@ -2,6 +2,7 @@ const CTA = () => {
   return (
     <section className='cta-section'>
       <div className='cta-badge'>Start learning your way.</div>
+      <h2>Build and Personalize Learning Companion</h2>
     </section>
   )
 }
