420be2f17951bf9a3289fbaac9ce3e7961a80a22
hrishikasamani <samani.hr@northeastern.edu>
2025-07-06 00:09:35 -0400

    edits

--- components/CompanionList.tsx
+++ components/CompanionList.tsx
@@ -6,10 +6,10 @@ import {
   TableHead,
   TableHeader,
   TableRow,
-} from '@/components/ui/table'
-import { cn, getSubjectColor } from '@/lib/utils';
-import Image from 'next/image';
-import Link from 'next/link';
+} from "@/components/ui/table"
+import {cn, getSubjectColor} from "@/lib/utils";
+import Link from "next/link";
+import Image from "next/image";
 
 interface CompanionsListProps {
   title: string;
@@ -17,36 +17,36 @@ interface CompanionsListProps {
   classNames?: string;
 }
 
-const CompanionList = ({title, companions, classNames}: CompanionsListProps) => {
+const CompanionsList = ({ title, companions, classNames }: CompanionsListProps) => {
   return (
     <article className={cn('companion-list', classNames)}>
-      <h2 className='font-bold text-3xl'>Recent Sessions</h2>
+      <h2 className="font-bold text-3xl">{title}</h2>
       <Table>
         <TableHeader>
           <TableRow>
-            <TableHead className='text-lg w-2/3'>Lessons</TableHead>
-            <TableHead className='text-lg'>Subject</TableHead>
-            <TableHead className='text-lg text-right'>Duration</TableHead>
+            <TableHead className="text-lg w-2/3">Lessons</TableHead>
+            <TableHead className="text-lg">Subject</TableHead>
+            <TableHead className="text-lg text-right">Duration</TableHead>
           </TableRow>
         </TableHeader>
         <TableBody>
           {companions?.map(({id, subject, name, topic, duration}) => (
             <TableRow key={id}>
               <TableCell>
-                <Link href={'/companions/${id}'}>
-                  <div className='flex items-center gap-2'>
-                    <div className='size-[72px] flex items-center justify-center rounded-lg max-md:hidden' style={{backgroundColor: getSubjectColor(subject)}}>
-                      <Image 
-                        src={'/icons/${subject}.svg'}
+                <Link href={`/companions/${id}`}>
+                  <div className="flex items-center gap-2">
+                    <div className="size-[72px] flex items-center justify-center rounded-lg max-md:hidden" style={{ backgroundColor: getSubjectColor(subject) }}>
+                      <Image
+                        src={`/icons/${subject}.svg`}
                         alt={subject}
                         width={35}
                         height={35} />
                     </div>
-                    <div className='flex-flex-col gap-2'>
-                      <p className='font-bold text-2xl'>
+                    <div className="flex flex-col gap-2">
+                      <p className="font-bold text-2xl">
                         {name}
                       </p>
-                      <p className='text-lg'>
+                      <p className="text-lg">
                         {topic}
                       </p>
                     </div>
@@ -54,16 +54,25 @@ const CompanionList = ({title, companions, classNames}: CompanionsListProps) => 
                 </Link>
               </TableCell>
               <TableCell>
-                <div className='subject-badge w-fit max-md:hidden'>
+                <div className="subject-badge w-fit max-md:hidden">
                   {subject}
                 </div>
-                <div className='flex items-center justify-center rounded-lg w-fit p-2 md:hidden' style={{backgroundColor: getSubjectColor(subject)}}>
+                <div className="flex items-center justify-center rounded-lg w-fit p-2 md:hidden" style={{backgroundColor: getSubjectColor(subject)}}>
                   <Image
-                    src={'/icons/${subject}.svg'}
+                    src={`/icons/${subject}.svg`}
                     alt={subject}
-                    width={15}
-                    height={15}
+                    width={18}
+                    height={18}
                   />
+                </div>
+              </TableCell>
+              <TableCell>
+                <div className="flex items-center gap-2 w-full justify-end">
+                  <p className="text-2xl">
+                    {duration} {' '}
+                    <span className="max-md:hidden">mins</span>
+                  </p>
+                    <Image src="/icons/clock.svg" alt="minutes" width={14} height={14} className="md:hidden" />
                 </div>
               </TableCell>
             </TableRow>
@@ -74,4 +83,4 @@ const CompanionList = ({title, companions, classNames}: CompanionsListProps) => 
   )
 }
 
-export default CompanionList
+export default CompanionsList;
