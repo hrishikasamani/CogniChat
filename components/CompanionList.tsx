241578ce1b1ca2ff5fbc2ec5ffe2760d94bb5aac
hrishikasamani <samani.hr@northeastern.edu>
2025-07-06 23:58:43 -0400

    edits

--- components/CompanionList.tsx
+++ components/CompanionList.tsx
@@ -42,8 +42,29 @@ const CompanionList = ({title, companions, classNames}: CompanionsListProps) => 
                         width={35}
                         height={35} />
                     </div>
+                    <div className='flex-flex-col gap-2'>
+                      <p className='font-bold text-2xl'>
+                        {name}
+                      </p>
+                      <p className='text-lg'>
+                        {topic}
+                      </p>
+                    </div>
                   </div>
                 </Link>
+              </TableCell>
+              <TableCell>
+                <div className='subject-badge w-fit max-md:hidden'>
+                  {subject}
+                </div>
+                <div className='flex items-center justify-center rounded-lg w-fit p-2 md:hidden' style={{backgroundColor: getSubjectColor(subject)}}>
+                  <Image
+                    src={'/icons/${subject}.svg'}
+                    alt={subject}
+                    width={15}
+                    height={15}
+                  />
+                </div>
               </TableCell>
             </TableRow>
           ))}
