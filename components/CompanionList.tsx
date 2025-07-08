9ed2fb20f32d0cfe4b5a66b2f7bab29dcb3c3a44
hrishikasamani <samani.hr@northeastern.edu>
2025-07-07 23:50:53 -0400

    edits

--- components/CompanionList.tsx
+++ components/CompanionList.tsx
@@ -7,7 +7,8 @@ import {
   TableHeader,
   TableRow,
 } from '@/components/ui/table'
-import { cn } from '@/lib/utils';
+import { cn, getSubjectColor } from '@/lib/utils';
+import Image from 'next/image';
 import Link from 'next/link';
 
 interface CompanionsListProps {
@@ -29,11 +30,19 @@ const CompanionList = ({title, companions, classNames}: CompanionsListProps) => 
           </TableRow>
         </TableHeader>
         <TableBody>
-          {companions?.map((companion) => (
-            <TableRow>
+          {companions?.map(({id, subject, name, topic, duration}) => (
+            <TableRow key={id}>
               <TableCell>
-                <Link href={'/companions/${companion.id}'}>
-                  {companion.subject}
+                <Link href={'/companions/${id}'}>
+                  <div className='flex items-center gap-2'>
+                    <div className='size-[72px] flex items-center justify-center rounded-lg max-md:hidden' style={{backgroundColor: getSubjectColor(subject)}}>
+                      <Image 
+                        src={'/icons/${subject}.svg'}
+                        alt={subject}
+                        width={35}
+                        height={35} />
+                    </div>
+                  </div>
                 </Link>
               </TableCell>
             </TableRow>
