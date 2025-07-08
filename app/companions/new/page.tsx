c55304f4158c998bc87bb72e8d35bf9c0cb585e0
hrishikasamani <samani.hr@northeastern.edu>
2025-07-08 11:20:51 -0400

    edits

--- app/companions/new/page.tsx
+++ app/companions/new/page.tsx
@@ -1,8 +1,11 @@
+import CompanionForm from "@/components/CompanionForm"
+
 const NewCompanion = () => {
   return (
     <main>
       <article>
         <h1>Companion Builder</h1>
+        <CompanionForm />
       </article>  
     </main>
   )
