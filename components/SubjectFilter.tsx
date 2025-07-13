24e3c665e9b4f82885586d4d93b0e89048d4eaa5
hrishikasamani <samani.hr@northeastern.edu>
2025-07-12 21:02:40 -0400

    updates

--- components/SubjectFilter.tsx
+++ components/SubjectFilter.tsx
@@ -1,6 +1,32 @@
-import React from 'react'
+'use client'
+import { useRouter, useSearchParams } from 'next/navigation'
+import React, { useEffect, useState } from 'react'
+import { formUrlQuery, removeKeysFromUrlQuery } from '@jsmastery/utils';
 
 const SubjectFilter = () => {
+  const router = useRouter();
+  const searchParams = useSearchParams();
+  const query = searchParams.get('subject') || '';
+
+  const [subject, setSubject] = useState(query);
+
+  useEffect(() => {
+    let newUrl = '';
+    if(subject === 'all') {
+      newUrl = removeKeysFromUrlQuery({
+        params: searchParams.toString(),
+        keysToRemove: ['subject'],
+      });
+    } else {
+      newUrl = formUrlQuery({
+        params: searchParams.toString(),
+        key: 'subject',
+        value: subject
+      });
+    }
+    router.push(newUrl, { scroll: false });
+  }, [subject]);
+
   return (
     <div>SubjectFilter</div>
   )
