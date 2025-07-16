f15f4976d81aef20a61895f928c75c62c2b3d52f
hrishikasamani <samani.hr@northeastern.edu>
2025-07-16 00:24:02 -0400

    destruct

--- app/companions/[id]/page.tsx
+++ app/companions/[id]/page.tsx
@@ -10,30 +10,29 @@ interface CompanionSessionPageProps {
 
 const CompanionSession = async ({ params }: CompanionSessionPageProps) => {
   const { id } = await params;
-  const companion = await getCompanion(id);
+  const { name, subject, title, topic, duration } = await getCompanion(id);
   const user = await currentUser();
 
   if (!user) redirect('/sign-in');
-  if (!companion) redirect('/companions');
-  console.log(companion);
+  if (!name) redirect('/companions');
 
   return (
     <main>
       <article className="flex rounded-border justify-between p-6 max-md:flex-col">
         <div className="flex items-center gap-2">
-          <div className="size-[72px] flex items-center justify-center rounded-lg max-md:hidden" style={{backgroundColor: getSubjectColor(companion.subject)}}>
-            <Image src={`/icons/${companion.subject}.svg`} alt={companion.subject} width={35} height={35} />
+          <div className="size-[72px] flex items-center justify-center rounded-lg max-md:hidden" style={{backgroundColor: getSubjectColor(subject)}}>
+            <Image src={`/icons/${subject}.svg`} alt={subject} width={35} height={35} />
           </div>
           <div className="flex flex-col gap-2">
             <div className="flex items-center gap-2">
               <p className="font-bold text-2xl">
-                {companion.name}
+                {name}
               </p>
               <div className="subject-badge max-sm:hidden">
-                {companion.subject}
+                {subject}
               </div>
             </div>
-            <p className="text-lg">{companion.topic}</p>
+            <p className="text-lg">{topic}</p>
           </div>
         </div>
       </article>
