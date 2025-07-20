296ee37a282074161de3336eec9cc5af4630ecc2
hrishikasamani <samani.hr@northeastern.edu>
2025-07-19 23:53:40 -0400

    edits

--- app/companions/[id]/page.tsx
+++ app/companions/[id]/page.tsx
@@ -1,8 +1,14 @@
+import { getCompanion } from "@/lib/actions/companion.actions";
+import { currentUser } from "@clerk/nextjs/server";
+
 interface CompanionSessionPageProps {
   params: Promise<{ id: string }>
 }
 
-const CompanionSession = ({ params }: CompanionSessionPageProps) => {
+const CompanionSession = async ({ params }: CompanionSessionPageProps) => {
+  const { id } = await params;
+  const companion = await getCompanion(id);
+  const user = await currentUser();
   return (
     <div>
       CompanionSession
