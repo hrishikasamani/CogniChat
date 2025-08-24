fe3e56df41b3e35fffc02a242517e4928c189164
hrishikasamani <samani.hr@northeastern.edu>
2025-08-24 18:57:09 -0400

    accordian

--- app/my-journey/page.tsx
+++ app/my-journey/page.tsx
@@ -1,8 +1,23 @@
+import {
+  Accordion,
+  AccordionContent,
+  AccordionItem,
+  AccordionTrigger,
+} from "@/components/ui/accordion"
+
 const Profile = () => {
   return (
-    <div>
-      Profile
-    </div>
+    <main className="min-lg:w-3/4">
+      <section></section>
+      <Accordion type="single" collapsible>
+        <AccordionItem value="item-1">
+          <AccordionTrigger>Is it accessible?</AccordionTrigger>
+          <AccordionContent>
+            Yes. It adheres to the WAI-ARIA design pattern.
+          </AccordionContent>
+        </AccordionItem>
+      </Accordion>
+    </main>
   )
 }
 
