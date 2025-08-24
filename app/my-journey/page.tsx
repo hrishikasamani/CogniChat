5c5dc4c6c2d885db273a8c153fdb79cdc5fe74c3
hrishikasamani <samani.hr@northeastern.edu>
2025-08-23 20:00:35 -0400

    profile page changes

--- app/my-journey/page.tsx
+++ app/my-journey/page.tsx
@@ -6,6 +6,7 @@ import {
 } from "@/components/ui/accordion"
 import { getUserCompanions, getUserSessions } from "@/lib/actions/companion.actions"
 import { currentUser } from "@clerk/nextjs/server"
+import Image from "next/image"
 import { redirect } from "next/navigation"
 
 const Profile = async () => {
@@ -15,10 +16,17 @@ const Profile = async () => {
 
   const companions = await getUserCompanions(user.id);
   const sessionHistory = await getUserSessions(user.id);
-  
+
   return (
     <main className="min-lg:w-3/4">
-      <section></section>
+      <section className="flex justify-between gap-4 max-sm:flex-col items-center">
+        <Image src={user.imageUrl} alt={user.firstName!} width={110} height={110} />
+        <div className="flex flex-col gap-2">
+          <h1 className="font-bold text-2xl">
+            {user.firstName} {user.lastName}
+          </h1>
+        </div>
+      </section>
       <Accordion type="single" collapsible>
         <AccordionItem value="item-1">
           <AccordionTrigger>Is it accessible?</AccordionTrigger>
