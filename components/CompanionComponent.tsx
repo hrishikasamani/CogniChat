a1eb4e82c6d3e640ca56ed9bc6390c9071b8c6dd
hrishikasamani <samani.hr@northeastern.edu>
2025-08-30 23:26:44 -0400

    updates

--- components/CompanionComponent.tsx
+++ components/CompanionComponent.tsx
@@ -1,6 +1,7 @@
 'use client'
 import { cn, getSubjectColor } from '@/lib/utils'
-import React, { useState } from 'react'
+import { vapi } from '@/lib/vapi.sdk';
+import React, { useEffect, useState } from 'react'
 
 enum CallStatus{
   INACTIVE = 'INACTIVE',
@@ -11,6 +12,24 @@ enum CallStatus{
 
 const CompanionComponent = ({ companionId, subject, topic, name, userName, userImage, style, voice }: CompanionComponentProps) => {
   const [callStatus, setCallStatus] = useState<CallStatus>(CallStatus.INACTIVE);
+  const [isSpeaking, setIsSpeaking] = useState(false);
+
+  useEffect(() => {
+    const onCallStart = () => setCallStatus(CallStatus.ACTIVE);
+    const onCallEnd = () => setCallStatus(CallStatus.FINISHED);
+    const onMessage = () => {}
+    const onSpeechStart = () => setIsSpeaking(true);
+    const onSpeechEnd = () => setIsSpeaking(false);
+    const onError = (error: Error) => console.log('Error', error);
+
+    vapi.on('call-start', onCallStart);
+    vapi.on('call-end', onCallEnd);
+    vapi.on('message', onMessage);
+    vapi.on('error', onError);
+    vapi.on('speech-start', onSpeechStart);
+    vapi.on('speech-end', onSpeechEnd);
+
+  }, []);
 
   return (
     <section className='flex flex-col h-[70vh]'>
