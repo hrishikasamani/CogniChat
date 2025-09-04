636efba3f737340fe38e34e1700bd41997e3135c
hrishikasamani <samani.hr@northeastern.edu>
2025-09-03 20:22:29 -0400

    query

--- components/SearchInput.tsx
+++ components/SearchInput.tsx
@@ -1,10 +1,15 @@
 'use client'
-import { useParams, useRouter } from 'next/navigation'
+import { useParams, useRouter, useSearchParams } from 'next/navigation'
+import { useState } from 'react';
 
 const SearchInput = () => {
   const params = useParams();
   const router = useRouter();
-  
+  const searchParams = useSearchParams();
+  const query = searchParams.get('topic') || '';
+
+  const [searchQuery, setSearchQuery] = useState('');
+
   return (
     <div>
       
