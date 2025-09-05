f8aa46e27b1adb81d523aa1348fd71387f0d00da
hrishikasamani <samani.hr@northeastern.edu>
2025-09-04 20:20:12 -0400

    updates

--- components/SearchInput.tsx
+++ components/SearchInput.tsx
@@ -1,8 +1,10 @@
 'use client'
-import { useParams } from 'next/navigation'
+import { useParams, useRouter } from 'next/navigation'
 
 const SearchInput = () => {
   const params = useParams();
+  const router = useRouter();
+  
   return (
     <div>
       
