5b06d7c3b91dd46d6d6a28e9f83d9f38b5635477
hrishikasamani <samani.hr@northeastern.edu>
2025-09-10 17:58:20 -0400

    updates

--- components/CompanionForm.tsx
+++ components/CompanionForm.tsx
@@ -5,10 +5,32 @@ import { useForm } from 'react-hook-form'
 import { z } from 'zod'
 
 const formSchema = z.object({
-    username: z.string().min(minLength: 2).max(maxLength: 50),
+    name: z.string().min(minLength: 1, { message: 'Companion is required' }),
+    subject: z.string().min(minLength: 1, { message: 'Subject is required' }),
+    topic: z.string().min(minLength: 1, { message: 'Topic is required' }),
+    voice: z.string().min(minLength: 1, { message: 'Voice is required' }),
+    style: z.string().min(minLength: 1, { message: 'Style is required' }),
+    duration: z.coerce.number().min( value: 1, { message: 'Duration is required.'}),
 })
 
 const CompanionForm = () => {
+
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      name,
+      subject,
+      topic
+      voice,
+      style
+      duration
+    },
+  })
+
+  function onSubmit(values: z.infer<typeof formSchema>) {
+    console.log(values)
+  }
+
   return (
     <div>
       CompanionForm
