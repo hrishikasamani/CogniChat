effc4a50dee5087ba0e634622153f3411b51090b
hrishikasamani <samani.hr@northeastern.edu>
2025-09-16 18:44:46 -0400

    mistake

---
+++ components/CompanionForm.tsx
@@ -0,0 +1,53 @@
+'use client'
+
+import { zodResolver } from '@hookform/resolvers/zod'
+import { useForm } from 'react-hook-form'
+import { z } from 'zod'
+import { Button } from '@/components/ui/button'
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from '@/components/ui/form'
+import { Input } from '@/components/ui/input'
+
+const formSchema = z.object({
+    name: z.string().min(minLength: 1, { message: 'Companion is required' }),
+    subject: z.string().min(minLength: 1, { message: 'Subject is required' }),
+    topic: z.string().min(minLength: 1, { message: 'Topic is required' }),
+    voice: z.string().min(minLength: 1, { message: 'Voice is required' }),
+    style: z.string().min(minLength: 1, { message: 'Style is required' }),
+    duration: z.coerce.number().min( value: 1, { message: 'Duration is required.'}),
+})
+
+const CompanionForm = () => {
+
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      name: '',
+      subject: '',
+      topic: '',
+      voice: '',
+      style: '',
+      duration: 15,
+    },
+  })
+
+  function onSubmit(values: z.infer<typeof formSchema>) {
+    console.log(values)
+  }
+
+  return (
+    <div>
+      CompanionForm is a good form!
+      Oh no!
+    </div>
+  )
+}
+
+export default CompanionForm
