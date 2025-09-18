c7e9641a56622d06b5c8edc1e7655a8af6250d63
hrishikasamani <samani.hr@northeastern.edu>
2025-09-17 22:51:09 -0400

    errors in companionForm

--- components/CompanionForm.tsx
+++ components/CompanionForm.tsx
@@ -2,7 +2,7 @@
 
 import { zodResolver } from '@hookform/resolvers/zod'
 import { useForm } from 'react-hook-form'
-import { z } from 'zod'
+import { minLength, z } from 'zod'
 import { Button } from '@/components/ui/button'
 import {
   Form,
@@ -19,12 +19,12 @@ import { subjects } from '@/constants'
 import { Textarea } from './ui/textarea'
 
 const formSchema = z.object({
-    name: z.string().min(minLength: 1, { message: 'Companion is required' }),
-    subject: z.string().min(minLength: 1, { message: 'Subject is required' }),
-    topic: z.string().min(minLength: 1, { message: 'Topic is required' }),
-    voice: z.string().min(minLength: 1, { message: 'Voice is required' }),
-    style: z.string().min(minLength: 1, { message: 'Style is required' }),
-    duration: z.coerce.number().min( value: 1, { message: 'Duration is required.'}),
+  name: z.string().min(1, { message: 'Companion is required' }),
+  subject: z.string().min(1, { message: 'Subject is required' }),
+  topic: z.string().min(1, { message: 'Topic is required' }),
+  voice: z.string().min(1, { message: 'Voice is required' }),
+  style: z.string().min(1, { message: 'Style is required' }),
+  duration: z.coerce.number().min(1, { message: 'Duration is required.' }),
 })
 
 const CompanionForm = () => {
