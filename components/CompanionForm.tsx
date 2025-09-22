8e97b9f11cab6d6880c8ac85aa05848dcd318929
hrishikasamani <samani.hr@northeastern.edu>
2025-09-22 16:34:43 -0400

    form

--- components/CompanionForm.tsx
+++ components/CompanionForm.tsx
@@ -43,9 +43,24 @@ const CompanionForm = () => {
   }
 
   return (
-    <div>
-      CompanionForm is a good form!
-    </div>
+    <Form {...form} >
+      <form onSubmit={form.handleSubmit(onSubmit)} className='space-y-8'>
+        <FormField
+          control={form.control}
+          name='name'
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Username</FormLabel>
+              <FormControl>
+                <Input placeholder='shadcn' {...field} />
+              </FormControl>
+              <FormDescription>
+                This is your public display name.
+              </FormDescription>
+              <FormMessage />
+            </FormItem>
+          )}
+      </form>
   )
 }
 
