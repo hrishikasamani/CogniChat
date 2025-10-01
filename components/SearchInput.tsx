df27ada6234725c9ff1dfec8a42443e1be19dff9
hrishikasamani <samani.hr@northeastern.edu>
2025-09-30 20:41:00 -0400

    search query

--- components/SearchInput.tsx
+++ components/SearchInput.tsx
@@ -2,7 +2,7 @@
 import Image from 'next/image';
 import { useParams, useRouter, useSearchParams } from 'next/navigation'
 import { useEffect, useState } from 'react';
-import { formUrlQuery } from '@jsmastery/utils';
+import { formUrlQuery, removeKeysFromUrlQuery } from '@jsmastery/utils';
 
 const SearchInput = () => {
   const pathname = useParams();
@@ -20,6 +20,14 @@ const SearchInput = () => {
         value: searchQuery
       });
       router.push(newUrl, { scroll: false });
+    } else {
+      if(pathname === '/companions') {
+        const newUrl = removeKeysFromUrlQuery({
+          params: searchParams.toString(),
+          keysToRemove: ['topic'],
+        });
+        router.push(newUrl, { scroll: false });
+      }
     }
   }, [searchQuery, router, searchParams, pathname]);
 
