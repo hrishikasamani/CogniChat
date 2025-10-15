efc4c6de4a6918995c85a3ea03db34d39d02e650
hrishikasamani <samani.hr@northeastern.edu>
2025-10-14 20:24:28 -0400

    image

--- components/SearchInput.tsx
+++ components/SearchInput.tsx
@@ -1,4 +1,5 @@
 'use client'
+import Image from 'next/image';
 import { useParams, useRouter, useSearchParams } from 'next/navigation'
 import { useState } from 'react';
 
@@ -11,8 +12,13 @@ const SearchInput = () => {
   const [searchQuery, setSearchQuery] = useState('');
 
   return (
-    <div>
-      
+    <div className='relative border border-black rounded-lg items-center flex gap-2 px-2 py-1 h-fit'>
+      <Image 
+        src='/icons/search.svg' 
+        alt='seach' 
+        width={15} 
+        height={15}
+      />
     </div>
   )
 }
