f34f2d183277556b933572c29c6f858862de2867
hrishikasamani <samani.hr@northeastern.edu>
2025-10-22 22:32:32 -0400

    reached limit

--- app/companions/new/page.tsx
+++ app/companions/new/page.tsx
@@ -2,6 +2,7 @@ import CompanionForm from "@/components/CompanionForm"
 import { newCompanionPermissions } from "@/lib/actions/companion.actions";
 import { auth } from "@clerk/nextjs/server";
 import Image from "next/image";
+import Link from "next/link";
 import { redirect } from "next/navigation";
 
 const NewCompanion = async () => {
@@ -21,10 +22,15 @@ const NewCompanion = async () => {
         </article>
       ): (
         <article className="companion-limit">
-          <Image src="images/limit.svg" alt="Companion limit reached" width={360} height={230} />
+          <Image src="/images/limit.svg" alt="Companion limit reached" width={360} height={230} />
           <div className="cta-badge">
             Upgrade your plan
           </div>
+          <h1>You've Reached Your Limit</h1>
+          <p>Ypu've reached your companion limit. Upgrade to create more companions and premium features.</p>
+          <Link href="/subscription" className="btn-primary w-full justify-center">
+            Upgrade My Plan
+          </Link>
         </article>
       )}
   
