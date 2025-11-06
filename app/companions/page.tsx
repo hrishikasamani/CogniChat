917ff65990885414ea4d436be3ad033f25a90596
hrishikasamani <samani.hr@northeastern.edu>
2025-11-06 18:35:29 -0500

    updates

--- app/companions/page.tsx
+++ app/companions/page.tsx
@@ -2,10 +2,8 @@ import { getAllCompanions } from "@/lib/actions/companion.actions";
 
 const CompanionsLibrary = async({ searchParams }: SearchParams) => {
 
-  const params = await searchParams;
+  const filters = await searchParams;
   const subject = filters.subject ? filters.subject : '';
-
-  console.log('PARAMS:', params);
 
   return (
     <div>
