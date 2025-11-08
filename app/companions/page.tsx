8513c3c61350cd30ff97de125e44015df4794e34
hrishikasamani <samani.hr@northeastern.edu>
2025-11-08 15:18:29 -0500

    search params

--- app/companions/page.tsx
+++ app/companions/page.tsx
@@ -1,4 +1,9 @@
-const CompanionsLibrary = () => {
+const CompanionsLibrary = async({ searchParams }: SearchParams) => {
+
+  const params = await searchParams;
+
+  console.log('PARAMS:', params);
+  
   return (
     <div>
       CompanionsLibrary
