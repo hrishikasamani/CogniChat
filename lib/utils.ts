20938790af46a07896a9673ccc2355a17b26d0cf
hrishikasamani <samani.hr@northeastern.edu>
2025-11-15 18:33:59 -0500

    sentry setup

--- lib/utils.ts
+++ lib/utils.ts
@@ -55,7 +55,7 @@ export const configureAssistant = (voice: string, style: string) => {
       ],
     },
     clientMessages: [],
-    serverMessages: "conversation-update",
+    serverMessages: [],
   };
   return vapiAssistant;
 };
